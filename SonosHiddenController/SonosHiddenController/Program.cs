269ca922c4ee96c685993cb1249c21a348e36b99
dedril <github@doddrell.com>
2014-12-28 14:53:01 -0500

    tired of false positives from myo, now only listens to poses if you are holding the ctrl key

--- SonosHiddenController/SonosHiddenController/Program.cs
+++ SonosHiddenController/SonosHiddenController/Program.cs
@@ -2,6 +2,10 @@
 using System.CodeDom;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleHotKey;
 using MyoSharp.Communication;
 using MyoSharp.Device;
 using MyoSharp.Poses;
@@ -41,68 +45,124 @@ namespace Myo
             PreviousTrack = 786432,
             NextTrack = 720896
         }
+
+        //need both VK control (for the key) and MOD control (the modifier)
+        //for the ctrl hot key to work.
+        private static uint VK_CONTROL = 0x11;
+        private static uint MOD_CONTROL = 0x0002;
+
+        private static bool CtrlKeyHeld = false;
+        //helps us measure if we are still holding the key
+        private static DateTime CtrlKeyLastPress = DateTime.MinValue;
         
         #region Methods
         public static void Main()
         {
-            // create a hub to manage Myos
-            using (var channel = Channel.Create(ChannelDriver.Create(ChannelBridge.Create())))
-            using (var hub = Hub.Create(channel))
+            int hotKeyId = 0;
+            try
             {
-                // listen for when a Myo connects
-                hub.MyoConnected += (sender, e) =>
+                //register a listener to watch for the left CTRL key being pressed.
+                //we are going to use it to minimize all the false positive poses from the myo.
+                hotKeyId = HotKeyManager.RegisterHotKey(VK_CONTROL, MOD_CONTROL);
+                HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressed);
+               
+                // create a hub to manage Myos
+                using (var channel = Channel.Create(ChannelDriver.Create(ChannelBridge.Create())))
+                using (var hub = Hub.Create(channel)) 
                 {
-                    Console.WriteLine("Myo {0} has connected!", e.Myo.Handle);
 
-                    e.Myo.PoseChanged += Myo_PoseChanged;
-                    e.Myo.Locked += Myo_Locked;
-                    e.Myo.Unlocked += Myo_Unlocked;
+                    // listen for when a Myo connects
+                    hub.MyoConnected += (sender, e) =>
+                    {
+                        Console.WriteLine("Myo {0} has connected!", e.Myo.Handle);
 
-                };
+                        e.Myo.PoseChanged += Myo_PoseChanged;
+                        e.Myo.Locked += Myo_Locked;
+                        e.Myo.Unlocked += Myo_Unlocked;
+                    };
 
-                // start listening for Myo data
-                channel.StartListening();
+                    // start listening for Myo data
+                    channel.StartListening();
 
-                while (true)
-                {
-                    //forever loop
+                    while (true)
+                    {
+                        //forever loop, listening to key events - this is necessary for the hotkey functionality.
+                        Console.ReadLine();
+                    }
                 }
+
+            }
+            finally
+            {
+                HotKeyManager.UnregisterHotKey(hotKeyId);
             }
         }
+      
         #endregion
 
         #region Event Handlers
+
+        private static void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
+        {
+            if (!CtrlKeyHeld)
+            {
+                Console.WriteLine(string.Format("hot key pressed key:{0}, modifier:{1}", e.Key, e.Modifiers));
+                CtrlKeyHeld = true;
+            }
+
+            //we need to update this each milisecond the ctrl key is held down, otherwise it will timeout quickly even if the user is holding it down
+            CtrlKeyLastPress = DateTime.Now;
+
+            //in a separate non-blocking thread, wait and see if the user lets go of the CTRL key
+            Task.Factory.StartNew(() => CheckIfUserStillHoldingHotKey());
+        }
+
+        private static void CheckIfUserStillHoldingHotKey()
+        {
+            Thread.Sleep(1000);
+            if (CtrlKeyHeld &&
+                TimeSpan.FromTicks(DateTime.Now.Ticks - CtrlKeyLastPress.Ticks).TotalMilliseconds > 1000)
+            {
+                //then we are no longer holding the key
+                CtrlKeyHeld = false;
+                Console.WriteLine("No longer holding down hot key");
+            }
+        }
+
         private static void Myo_PoseChanged(object sender, PoseEventArgs e)
         {
-            Console.WriteLine("{0} arm Myo detected {1} pose!", e.Myo.Arm, e.Myo.Pose);
-
-            switch (e.Myo.Pose)
+            if (CtrlKeyHeld)
             {
-                case Pose.FingersSpread:
-                    PauseSpotify();
-                    break;
+                Console.WriteLine("{0} arm Myo detected {1} pose!", e.Myo.Arm, e.Myo.Pose);
 
-                case Pose.WaveOut:
-                    if (e.Myo.Arm == Arm.Right)
-                    {
-                        NextTrackSpotify();
-                    }
-                    else
-                    {
-                        PreviousTrackSpotify(); 
-                    }
-                    break;
+                switch (e.Myo.Pose)
+                {
+                    case Pose.FingersSpread:
+                        PauseSpotify();
+                        break;
 
-                case Pose.WaveIn:
-                    if (e.Myo.Arm == Arm.Right)
-                    {
-                        PreviousTrackSpotify();
-                    }
-                    else
-                    {
-                        NextTrackSpotify();
-                    }
-                    break;
+                    case Pose.WaveOut:
+                        if (e.Myo.Arm == Arm.Right)
+                        {
+                            NextTrackSpotify();
+                        }
+                        else
+                        {
+                            PreviousTrackSpotify();
+                        }
+                        break;
+
+                    case Pose.WaveIn:
+                        if (e.Myo.Arm == Arm.Right)
+                        {
+                            PreviousTrackSpotify();
+                        }
+                        else
+                        {
+                            NextTrackSpotify();
+                        }
+                        break;
+                }
             }
         }
 
